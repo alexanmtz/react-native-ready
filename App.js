51efe4a8d8bd439860dbbf8ba2f917f75de0b6dd
Laerte Alves <laerte.alves14@outlook.com.br>
2018-08-04 13:36:37 -0400

    Add splash

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { AppRegistry, View, Text, ScrollView } from 'react-native';
+import { AppRegistry, View, Text, ScrollView, StyleSheet, Image, children } from 'react-native';
 import { Header } from 'react-native-elements';
 import { AppLoading } from "expo";
 import store from './store';
 import AppNavigation from './Navigation';
 import { initializeAssets } from "./assets";
+// Import Main Service
+import MainService from './app/Services/mainservice'
 
 export default class App extends Component {
+    state = {
+        loaded: false
+    }
 
-    constructor(props) {
+   constructor(props) {
         super(props);
         this.state = {
             assetsReady: false
         };
+        MainService.load(v => this.setState({loaded: true}));
     }
 
     componentDidMount() {
@@ -23,19 +29,43 @@ export default class App extends Component {
     }
 
     render() {
+        /*
+        return (
+            <View style={style.container}>
+                {this.state.loaded ? <Text>Welcome!</Text> : <Text>Carregando...</Text>}
+            </View>
+        )*/
+
         if (!this.state.assetsReady) {
             return (<AppLoading />);
         }
         
-        return (
-            <Provider store={store}>
-                <View style={{ flex: 1}}>
-                    <Header
-                        centerComponent={{ text: 'React Native Ready', style: { color: '#fff' } }} />
-                    <AppNavigation />
-                </View>
-            </Provider>
-        );
+        if (this.state.loaded){
+            return (
+                <Provider store={store}>
+                    <View style={{ flex: 1}}>
+                        <Header
+                            centerComponent={{ text: 'React Native Ready', style: { color: '#fff' } }} />
+                        <AppNavigation />
+                    </View>
+                </Provider>
+            );
+        } else {
+            return(
+                <Image
+                    style={{
+                        flex: 1,
+                        alignSelf: 'stretch',
+                        width: undefined,
+                        height: undefined
+                    }}
+                    source={require('./assets/images/splash.png')}
+                    >
+                    { children }
+                </Image>
+                //<Image source={require('./assets/images/splash.png')} style={{width: 100, height: 100}} />
+            )
+        }
     }
 }
 
---
+++ app/Services/mainservice.js
@@ -0,0 +1,5 @@
+export default class MainService{
+   static load(cb){
+        setTimeout(cb, 3000);
+    }
+}
